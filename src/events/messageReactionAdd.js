1269e6c03b8c7a55b56896582b75cff93717b377
MadCow234 <madcow234@gmail.com>
2020-06-24 22:21:36 -0500

    Stop checking bot's roles when they receive reactions

--- src/events/messageReactionAdd.js
+++ src/events/messageReactionAdd.js
@@ -1,6 +1,6 @@
 import { newReactionHistoryEmbed } from '../templates/embeds'
 import { userHasRoleForGuild }     from '../services/roleService'
-import { enums }                   from '../application'
+import {enums, mainContext} from '../application'
 import log                         from 'winston'
 
 /**
@@ -11,6 +11,9 @@ import log                         from 'winston'
 exports.run = async (reaction, user) => {
     try {
         let reactionInstance = (reaction.partial) ? await reaction.fetch() : reaction
+
+        // Don't respond to bots...unless I'm the one talking
+        if (reaction.message.author.bot && reaction.message.author.id !== mainContext.client.user.id) return
 
         if (await userHasRoleForGuild(reactionInstance.message.author, enums.roles.historyEnabled, reactionInstance.message.guild)) {
             await reactionInstance.message.author.send(newReactionHistoryEmbed(reactionInstance, user, true))
